7d8c75c42eae8ae673f669ff2f63d70ca1443147
jhc0510 <134457840+jhc0510@users.noreply.github.com>
2023-07-27 21:40:43 +0900

    Add files via upload

--- greet/list.php
+++ greet/list.php
@@ -13,7 +13,7 @@
 	$find = $_POST['find'];
 	$search = $_POST['search'];
 	}
-	$mode = "";
+	//$mode = "";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
