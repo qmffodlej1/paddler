01a5781974992942c28043ca558c5c304aac5105
qmffodlej1 <140581561+qmffodlej1@users.noreply.github.com>
2023-07-27 17:21:29 +0900

    Add files via upload

--- member/member_form.php
+++ member/member_form.php
@@ -7,15 +7,15 @@
     <style>
         /* 가운데 정렬을 위한 스타일링 */
         .center-table {
-		margin-left: 200px;
+            margin-left: 200px;
         }
     </style>
 <script>
     function check_id()
     {
         window.open("check_id.php?id=" + document.member_form.id.value,
-        "IDcheck",
-        "left=200,top=200,width=200,height=60,scrollbars=no,resizable=yes");
+            "IDcheck",
+            "left=200,top=200,width=200,height=60,scrollbars=no,resizable=yes");
     }
 
     function check_nick()
@@ -28,50 +28,43 @@
     function check_input()
     {
         if (!document.member_form.id.value) {
-            alert("아이디를 입력하세요");    
+            ("아이디를 입력하세요")
             document.member_form.id.focus();
-            return;
+            return false;
         }
-
         if (!document.member_form.pass.value) {
             alert("비밀번호를 입력하세요");    
             document.member_form.pass.focus();
-            return;
+            return false;
         }
-
         if (!document.member_form.pass_confirm.value) {
             alert("비밀번호확인을 입력하세요");    
             document.member_form.pass_confirm.focus();
-            return;
+            return false;
         }
-
         if (!document.member_form.name.value) {
             alert("이름을 입력하세요");    
             document.member_form.name.focus();
-            return;
+            return false;
         }
-
         if (!document.member_form.nick.value) {
             alert("닉네임을 입력하세요");    
             document.member_form.nick.focus();
-            return;
+            return false;
         }
-
-
         if (!document.member_form.hp2.value || !document.member_form.hp3.value ){
             alert("휴대폰 번호를 입력하세요");    
             document.member_form.nick.focus();
-            return;
+            return false;
         }
-
         if (document.member_form.pass.value != document.member_form.pass_confirm.value) {
             alert("비밀번호가 일치하지 않습니다.\n다시 입력해주세요.");    
             document.member_form.pass.focus();
             document.member_form.pass.select();
-            return;
+            return false;
         }
-
-    document.member_form.submit();
+        document.member_form.submit();
+        return true;
     }
 
     function reset_form() {
@@ -187,7 +180,7 @@
 </dev>
 		<div id="button">
             <a href="#">
-                <img src="../img/button_save.gif"  onclick="check_input()">
+                <img src="../img/button_save.gif"  onclick="check_input().call(this)">
             </a>&nbsp;&nbsp;
 		    <a href="#"><img src="../img/button_reset.gif" onclick="reset_form()"></a>
 		</div>
