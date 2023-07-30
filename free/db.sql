297a7e2f19dea1e7236f0e6a618638fa235c1fec
jhc0510 <134457840+jhc0510@users.noreply.github.com>
2023-07-30 15:18:46 +0900

    Update db.sql

--- free/db.sql
+++ free/db.sql
@@ -1,26 +1,3 @@
-create table concert (
-   num int not null auto_increment,
-   id char(15) not null,
-   name  char(10) not null,
-   nick  char(10) not null,
-   subject char(100) not null,
-   content text not null,
-   regist_day char(20),
-   hit int,
-   is_html char(1),
-   file_name_0 char(40),
-   file_name_1 char(40),
-   file_name_2 char(40),
-   file_name_3 char(40),
-   file_name_4 char(40),
-   file_copied_0 char(30),
-   file_copied_1 char(30),
-   file_copied_2 char(30),
-   file_copied_3 char(30),
-   file_copied_4 char(30), 
-   primary key(num)
-);
-
 create table download (
    num int not null auto_increment,
    id char(15) not null,
@@ -69,7 +46,8 @@ create table free (
    file_copied_3 char(30),
    file_copied_4 char(30), 
    primary key(num)
-);create table free_ripple (
+);
+create table free_ripple (
    num int not null auto_increment,
    parent int not null,
    id char(15) not null,
@@ -123,28 +101,3 @@ create table memo_ripple (
    regist_day char(20),
    primary key(num)
 );
-
-create table qna (
-   num int not null auto_increment,
-   group_num int not null,
-   depth int not null,
-   ord int not null,
-   id char(15) not null,
-   name  char(10) not null,
-   nick  char(10) not null,
-   subject char(100) not null,
-   content text not null,
-   regist_day char(20),
-   hit int,
-   is_html char(1),
-   primary key(num)
-);
-
-create table survey(
-   ans1 int,
-   ans2 int,
-   ans3 int,
-   ans4 int
-   );
-
-insert into survey values(0, 0, 0, 0);
