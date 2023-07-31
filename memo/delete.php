f547448ab90b7153a6edc2693ca81296ac4215a0
jinjinU <134563808+jinjinU@users.noreply.github.com>
2023-07-31 14:11:22 +0900

    Add files via upload

--- memo/delete.php
+++ memo/delete.php
@@ -1,15 +1,38 @@
-<?
-   include "../lib/dbconn.php";
-   $num = $_GET['num'];
-   $sql = "delete from memo where num = $num";
-   $result = $connect->query($sql);
+<?php
+session_start();
+if (!isset($_SESSION['userid'])) {
+    echo "<script>alert('로그인 후 이용해주세요.');history.go(-1);</script>";
+    exit;
+}
 
-   $connect->close();  
+if (isset($_GET['num'])) {
+    $num = $_GET['num'];
+} else {
+    echo "<script>alert('잘못된 접근입니다.');history.go(-1);</script>";
+    exit;
+}
 
-   echo "
-	   <script>
-	    location.href = 'memo.php';
-	   </script>
-	";
+include "../lib/dbconn.php";
+
+// 사용자가 삭제 권한이 있는지 확인합니다.
+$sql = "SELECT id FROM memo WHERE num='$num'";
+$result = $connect->query($sql);
+$row = $result->fetch_array(MYSQLI_ASSOC);
+$memo_id = $row['id'];
+
+$userid = $_SESSION['userid'];
+if ($userid != "admin" && $userid != $memo_id) {
+    echo "<script>alert('권한이 없습니다.');history.go(-1);</script>";
+    exit;
+}
+
+// 삭제 확인 창을 띄웁니다.
+echo "<script>
+    const confirmed = confirm('이 항목을 삭제하시겠습니까?');
+    if (confirmed) {
+        window.location.href = 'delete_comment_process.php?num=$num';
+    } else {
+        history.go(-1);
+    }
+</script>";
 ?>
-
---
+++ memo/delete_ripple_process.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if (!isset($_SESSION['userid'])) {
+    echo "<script>alert('로그인 후 이용해주세요.');history.go(-1);</script>";
+    exit;
+}
+
+if (isset($_GET['num'])) {
+    $num = $_GET['num'];
+} else {
+    echo "<script>alert('잘못된 접근입니다.');history.go(-1);</script>";
+    exit;
+}
+
+include "../lib/dbconn.php";
+
+// 사용자가 삭제 권한이 있는지 확인합니다.
+$sql = "SELECT id FROM memo_ripple WHERE num='$num'";
+$result = $connect->query($sql);
+$row = $result->fetch_array(MYSQLI_ASSOC);
+$memo_id = $row['id'];
+
+$userid = $_SESSION['userid'];
+if ($userid != "admin" && $userid != $memo_id) {
+    echo "<script>alert('권한이 없습니다.');history.go(-1);</script>";
+    exit;
+}
+
+// 선택한 댓글을 데이터베이스에서 삭제합니다.
+$sql = "DELETE FROM memo_ripple WHERE num='$num'";
+$result = $connect->query($sql);
+
+if ($result) {
+    echo "<script>alert('댓글이 삭제되었습니다.');window.location.href='memo.php';</script>";
+} else {
+    echo "<script>alert('댓글 삭제에 실패했습니다.');history.go(-1);</script>";
+}
+
+$connect->close();
+?>
